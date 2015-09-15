def37ed0fc0bee27141446189ef3c2fd65576b1f
Shaganpreet <c0663932@mylambton.ca>
2015-09-15 16:20:31 -0400

    Java Development Assignment 1

--- src/cpd3314/assign1/CPD3314Assign1.java
+++ src/cpd3314/assign1/CPD3314Assign1.java
@@ -17,6 +17,28 @@ public class CPD3314Assign1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        /**************
+         * Assignment-1
+         * Job posting
+         * Senior Java Developer
+         * TekHunter - Greater Toronto Area, ON
+         * http://www.indeed.ca/cmp/TekHunter.Biz/jobs/Senior-Java-Developer-c7833cb993f81134?sjdu=vQIlM60yK_PwYat7ToXhk1EQcCGKPDd26qp_pQ6XvqP_pcp8YDdFNCmGmAtCVeR-mnfcFugo4DZm2iJOHSOQjg
+         * Skills
+         * JDBC - Java database connectivity technology -https://en.wikipedia.org/wiki/Java_Database_Connectivity
+         * JUnit -  unit testing framework -https://en.wikipedia.org/wiki/JUnit
+         * JSP - create dynamically generated web pages -https://en.wikipedia.org/wiki/JavaServer_Pages
+         * AJAX - interrelated Web development techniques -https://en.wikipedia.org/wiki/Ajax_(programming)
+         * jQuery -cross-platform JavaScript library -https://en.wikipedia.org/wiki/JQuery
+         * JavaScript - interpreted programming language -https://en.wikipedia.org/wiki/JavaScript
+         * 
+         * Salary 
+         * Toronto - $60K
+         * Vancouver - $70K
+         * Waterloo - $68K
+         * 
+         * 
+         */
+         
     }
     
 }
